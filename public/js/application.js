b4b33f0a67b8208c6ebc57dceaeedf7395fb4e79
Aen <hello@aentan.com>
2014-07-11 19:53:03 +0800

    auto commit (mixture)

---
+++ public/js/application.js
@@ -0,0 +1,8 @@
+$(function() {
+
+  $('#subscribe-form').submit(function() {
+    window.open('https://tinyletter.com/aenism', 'popupwindow', 'scrollbars=yes,width=800,height=600');
+    return true;
+  });
+
+});
